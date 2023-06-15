5f791fd8fa9f4d7dcd31de3140c4962414016d8b
egarza27 <elizabethigarza10@gmail.com>
2023-06-14 20:03:41 -0500

    initial commit

--- src/components/Import.js
+++ src/components/Import.js
@@ -1,11 +1,68 @@
-import React from 'react'
+import React, { useState } from "react";
+import {
+  Button,
+  Table,
+  TableHead,
+  TableBody,
+  TableRow,
+  TableCell,
+  Container,
+  Menu,
+  MenuItem,
+} from "@mui/material";
+
+import { MoreVert } from "@mui/icons-material";
 
 const Import = (props) => {
-    // fill out this component
+  const [anchorEl, setAnchorEl] = useState(null);
 
-    return (
-        <p>Import Component</p>
-    )
-}
+  const handleClick = (e, makeId) => {
+    setAnchorEl(e.currentTarget);
+  };
 
-export default Import
+  const handleDelete = (index) => {
+    props.removeMake(index);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  return (
+    <Container>
+      <br></br>
+      <br></br>
+      <Button variant="contained" color="primary" onClick={props.fetchMakes}>
+        Import
+      </Button>
+      <h2>COUNT: {props.makes.length}</h2>
+      <Table sx={{ minWidth: 650 }} aria-label="simple table">
+        <TableHead>
+          <TableRow>
+            <TableCell>Id</TableCell>
+            <TableCell>Make</TableCell>
+            <TableCell>Actions</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {props.makes.map((make, idx) => (
+            <TableRow key={idx}>
+              <TableCell>{make.MakeId}</TableCell>
+              <TableCell>{make.MakeName}</TableCell>
+              <TableCell>
+                <MoreVert
+                  onClick={(e) => handleClick(e, make.MakeId)}
+                ></MoreVert>
+              </TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+      <Menu anchorEl={anchorEl} open={Boolean(anchorEl)} onClose={handleClose}>
+        <MenuItem onClick={handleDelete}>Delete</MenuItem>
+      </Menu>
+    </Container>
+  );
+};
+
+export default Import;
--- src/containers/Import.js
+++ src/containers/Import.js
@@ -1,1 +1,18 @@
-// fill out this container
+import { connect } from "react-redux";
+import Import from "../components/Import";
+import { fetchMakes, removeMake } from "../redux/actions";
+
+const mapStateToProps = (state) => {
+  return {
+    makes: state.makes,
+  };
+};
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    fetchMakes: () => dispatch(fetchMakes()),
+    removeMake: (index) => dispatch(removeMake(index)),
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Import);
