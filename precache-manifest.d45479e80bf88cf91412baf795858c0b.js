f0c0bb9c48c20047fef35636c500da349d738c72
egarza27 <elizabethigarza10@gmail.com>
2023-06-14 20:06:31 -0500

    Updates

---
+++ precache-manifest.d45479e80bf88cf91412baf795858c0b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "43ecd1602b5f4652f2dbacbe28355ae2",
+    "url": "/JS4111_ReduxThunk/index.html"
+  },
+  {
+    "revision": "adbca8c16dd02b69ef78",
+    "url": "/JS4111_ReduxThunk/static/css/main.71035251.chunk.css"
+  },
+  {
+    "revision": "827a673f805f844ddfeb",
+    "url": "/JS4111_ReduxThunk/static/js/2.710e96d3.chunk.js"
+  },
+  {
+    "revision": "adbca8c16dd02b69ef78",
+    "url": "/JS4111_ReduxThunk/static/js/main.df56fd4d.chunk.js"
+  },
+  {
+    "revision": "d74d00a4c807c259281f",
+    "url": "/JS4111_ReduxThunk/static/js/runtime~main.584a0d6e.js"
+  }
+]);
